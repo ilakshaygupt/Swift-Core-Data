89e4850b4548f591fff2c500712b269c82161777
lakshay <gupta22154006@akgec.ac.in>
2024-12-10 00:47:39 +0530

    Initial Commit

---
+++ Swift-Core-Data/Swift_Core_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Core_DataApp.swift
+//  Swift-Core-Data
+//
+//  Created by Lakshay Gupta on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Core_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
